5b80a6609a6dc78c46105a809ee92bea4750d93a
anesthetice <anesthetice>
2023-09-17 12:05:19 +0200

    ran clippy and fmt, very minor fixes

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,5 @@
-use serde::{
-    Serialize,
-    Deserialize
-};
-use serde_json;
-use std::{
-    io,
-    fs::read,
-};
+use serde::{Deserialize, Serialize};
+use std::{fs::read, io};
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
     pub number_of_players: usize,
--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,13 @@
-use std::io::{self, Read};
 use crossterm::{
-    execute,
     cursor,
-    terminal::{Clear, ClearType},
+    event::{self, KeyEventKind},
+    execute,
     style::Stylize,
-    event::{self, KeyEventKind}
+    terminal::{Clear, ClearType},
 };
+use std::io;
 
-const INTERLUDE_STR: &'static str = r"
+const INTERLUDE_STR: &str = r"
 / / / / / / / / / / / / / / / / / / / / / / / / /
 
             PRESS ANY KEY TO CONTINUE            
@@ -16,19 +16,28 @@ const INTERLUDE_STR: &'static str = r"
 ";
 
 pub fn lexicon_choose_error() -> io::Error {
-    io::Error::new(io::ErrorKind::Other, "Failed to choose a word from the lexicon")
+    io::Error::new(
+        io::ErrorKind::Other,
+        "Failed to choose a word from the lexicon",
+    )
 }
 
 pub fn interlude() -> io::Result<()> {
     println!("{}", INTERLUDE_STR.dark_yellow());
     loop {
         if event::poll(std::time::Duration::from_millis(100))? {
-            if let Ok(event::Event::Key(key)) = event::read() {   
-                if key.kind == KeyEventKind::Press {break;}
+            if let Ok(event::Event::Key(key)) = event::read() {
+                if key.kind == KeyEventKind::Press {
+                    break;
+                }
             }
         }
     }
-    execute!(std::io::stdout(), Clear(ClearType::All), cursor::MoveTo(0, 0))?;
+    execute!(
+        std::io::stdout(),
+        Clear(ClearType::All),
+        cursor::MoveTo(0, 0)
+    )?;
     Ok(())
 }
 
@@ -36,12 +45,18 @@ pub fn pause() -> io::Result<()> {
     println!("{}", "Press any key to continue...".dark_grey());
     loop {
         if event::poll(std::time::Duration::from_millis(100))? {
-            if let Ok(event::Event::Key(key)) = event::read() {   
-                if key.kind == KeyEventKind::Press {break;}
+            if let Ok(event::Event::Key(key)) = event::read() {
+                if key.kind == KeyEventKind::Press {
+                    break;
+                }
             }
         }
     }
-    execute!(std::io::stdout(), Clear(ClearType::All), cursor::MoveTo(0, 0))?;
+    execute!(
+        std::io::stdout(),
+        Clear(ClearType::All),
+        cursor::MoveTo(0, 0)
+    )?;
     Ok(())
 }
 
